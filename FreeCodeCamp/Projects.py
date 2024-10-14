103459e434beea23bb6966c4a99daa884987bb66
Sandeep Kumar Mishra <sandeepk.mishra@nettantra.net>
2024-10-14 19:02:30 +0530

    test

--- FreeCodeCamp/Projects.py
+++ FreeCodeCamp/Projects.py
@@ -277,3 +277,7 @@ if __name__ == '__main__':
     print('Generated password:', new_password)
     
 # ----------------------------------------------------------------------------------------------------------------------------------------
+
+# Algorithms are step-by-step procedures that developers use to perform calculations and solve computational problems.
+
+# In this project, you'll learn how to use functions, loops, conditional statements, and dictionary comprehensions to implement a Shortest Path algorithm.
